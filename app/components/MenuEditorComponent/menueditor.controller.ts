786c24fd2c90aff3d4ffb781b99ed20ffd23c532
Sam Waters <sam@samwaters.com>
2016-11-04 22:51:26 +0000

    Throttling on keyup events to prevent spamming the store too much, _ and types to support thottling

--- app/components/MenuEditorComponent/menueditor.controller.ts
+++ app/components/MenuEditorComponent/menueditor.controller.ts
@@ -1,23 +1,25 @@
 import {MenuService} from '../../services/menu.service';
+import * as _ from 'lodash';
 require('./menueditor.scss');
 
 export class MenuEditorController {
   public static id:string = 'menueditor.controller';
   public static $inject = ['$scope', MenuService.id];
 
+  public brand:Function;
+  public keyUp:Function;
+
   constructor($scope, private menuService) {
     this.menuService.registerServiceBindings($scope, this, {'menu':'menu'});
-  }
-
-  public brand(event) {
-    this.menuService.updateBrand(event.target.value);
+    this.brand = _.throttle((event) => {
+      this.menuService.updateBrand(event.target.value);
+    });
+    this.keyUp = _.throttle((item, event, field) => {
+      this.menuService.updateMenu(item.id, field, event.target.value);
+    }, 250);
   }
 
   public checkbox(item, event) {
     this.menuService.updateMenu(item.id, 'show', event.target.checked);
-  }
-
-  public keyUp(item, event, field) {
-    this.menuService.updateMenu(item.id, field, event.target.value);
   }
 }
