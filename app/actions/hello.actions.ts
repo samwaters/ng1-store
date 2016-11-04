11fc9922e280ca0482fb68ebe5156f709926e5e5
Sam Waters <sam@samwaters.com>
2016-11-04 17:26:40 +0000

    Remove unnecessary constructor and inject

--- app/actions/hello.actions.ts
+++ app/actions/hello.actions.ts
@@ -3,13 +3,6 @@ export class HelloActions {
   public static actions = {
     UPDATE_GREETING: 'UPDATE_GREETING'
   };
-  public static $inject = ['$ngRedux'];
-
-  constructor() {
-    return {
-      updateGreeting: this.updateGreeting
-    };
-  }
 
   public updateGreeting(newGreeting:string) {
     return (dispatch) => {
